b33492f8e7d6ca67dd01ce04d9624b0741b73f6a
nicolas <nicolas.lasportes@gmail.com>
2018-10-03 11:55:58 +0200

    init n°2

---
+++ animal.class.js
@@ -0,0 +1,10 @@
+function Animal(hp, appearance, regime, sex, dmg, age, maturity)
+{
+    this.hp = hp;
+    this.appearance = appearance;
+    this.regime = regime;
+    this.sex = sex;
+    this.dmg = dmg;
+    this.age = age;
+    this.maturity = maturity;
+}
