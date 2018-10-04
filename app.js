d999e14c6a072b5be90ff490e7aa12ff612bb75a
benjaminsandras <bensand77@hotmail.fr>
2018-10-04 09:03:53 +0200

    try draw circle

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ function board (x, y)
 {     
 	for (var i = 0; i < x; i++) 
 	{                   
-		$("#grid").append( '<tr data-id="'+i+'"></tr>');                     
+		$("#grid").append( '<tr id="'+i+'"></tr>');                     
 		for (var j = 0; j < y; j++) 
 		{                       
-			$("#grid tr:last-child").append( '<td data-id="'+i+'/'+j+'">'+'</td>' );
+			$("#grid tr:last-child").append( '<td id="'+i+'/'+j+'">'+'</td>' );
 
 		}          
 	}  
 }
 
 board(x,y);
+
+function canvas(coucou){
+	
+$("# '"+ coucou + "'").append("<canvas id='canv'></canvas>");
+
+	// var c=document.getElementById("canv");
+	// var ctx=c.getContext("2d");
+	// ctx.beginPath();
+	// ctx.arc(100,75,50,0,2*Math.PI);
+	// ctx.stroke();
+
+}
+
+canvas(20/5);
 
